c2d9de6b09a6d1e946765fc4cbb26016dccce5dd
NinaNinochka <ninamishchenko31@gmail.com>
2018-12-29 11:07:12 -0500

    New Jenkins practice

--- src/test/java/com/Maven/HomeWork1.java
+++ src/test/java/com/Maven/HomeWork1.java
@@ -3,18 +3,22 @@ package com.Maven;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class HomeWork1 {
 
-	public static void main(String[] args) {
+	@Test
+	public  void main() {
 	
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ninam\\Desktop\\Documents\\JARfiles\\Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ninam\\eclipse-workspace\\com.Maven\\Drivers\\chromedriver.exe");
 	       WebDriver driver = new ChromeDriver();
            String url="https://www.google.com/";
            driver.get(url);
            String title  =driver.getTitle();
            
            Assert.assertEquals("Google", title);
+           
+           driver.quit();
           
            
 	
